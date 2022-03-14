3776ad70c645a4a630043233bc6dcfad467e7928
Henrique Rocha <henrique.rocha@gmail.com>
2022-03-14 12:03:14 -0400

    Lecture 21 - March 14 Adding Get One operation

--- SingleFileServer.js
+++ SingleFileServer.js
@@ -21,6 +21,29 @@ app.get('/user',function(req,res){ // REST get (all) method
     res.end(); 
 });
 
+app.get('/user/:id',function(req,res){ //REST get (one) method
+    //URL parameter always on req.params.<name>
+    let id = parseInt( req.params.id ); //get param and convert to int
+    let found = null; //the found user
+    for(let i=0; i<users.length; i++){ //iterate the array looking for the id
+        if(users[i]._id === id){ //found a match
+            found = users[i]; //copy the user to found
+            break; //stop the loop
+        }
+    }
+
+    if(found !== null){ //We found the requested user
+        res.status(200); //200 = OK
+        res.send(found); //Send the found user
+    }
+    else{ //The requested id does not exist
+        res.status(404); //404 = Not Found
+        res.send({msg:'User not found.'}); //send a message
+    }
+
+    res.end(); //ends the response (only 1 end per response)
+});
+
 app.listen(port,hostname,function(){ // Listen to client requests in hostname:port
     console.log(`Server Running on ${hostname}:${port}...`);
 });
