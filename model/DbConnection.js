324ae6ad8d0f686fef68d8edc646018c91bff569
Henrique Rocha <henrique.rocha@gmail.com>
2022-04-04 16:26:30 -0400

    CI connection string

--- model/DbConnection.js
+++ model/DbConnection.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 exports.connect = function(where){
     let uri = process.env.DB_URI; //production DB
     if(where==='test') uri = process.env.TESTDB_URI; //Test DB
+    if(process.env.CI) uri = 'mongodb://adm:secret@localhost:27017';
 
     mongoose.connect(uri,function(err){
         if(err) console.log(err);
