c3e33191eb01b73e3f410f76e436b7ed1cac6f6d
Henrique Rocha <henrique.rocha@gmail.com>
2022-04-20 12:57:12 -0400

    Lecture 35 - April 20 created react04.html to show example of using events Used create-react-app to make a view folder (react environment) Created view/public/index.html with a basic page for react to render Created view/src/index.jsx with a simple Rendering Created Jumbotron.jsx as a simple react component

---
+++ view/src/MainPage/Jumbotron.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Jumbotron(){
+    return(   
+    <div className="p-5 bg-primary text-white text-center">
+       <h1>Web Programming Demo App</h1>
+    </div>
+    );
+}
---
+++ view/src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Jumbotron from "./MainPage/Jumbotron";
+
+ReactDOM.render(<Jumbotron />,
+    document.getElementById('root'));
